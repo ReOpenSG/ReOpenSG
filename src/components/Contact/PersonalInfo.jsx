31a7eddda18ca719ca48b05dd657a7659b3fa0eb
ingbinsee <ingbinsee@gmail.com>
2023-12-22 14:41:07 +0900

    🏛️ [Style] : 인적정보 기입란 배치 변경
    인적정보 기입란의 배열이 특정 분기점에서 틀어짐에 따라 배치를 변경하였습니다.

--- src/components/Contact/PersonalInfo.jsx
+++ src/components/Contact/PersonalInfo.jsx
@@ -8,56 +8,60 @@ function PersonalInfo() {
   const emailCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return (
-    <fieldset className={styles.personalInfoWrapper}>
-      <div className={styles.regWrapper}>
-        <FillingInput
-          labelText="소속"
-          inputType="text"
-          name="office"
-          placeholderText="회사 또는 부서를 입력해 주세요."
-        />
+    <fieldset className="py-open-2xl border-b -border--open-gray-200 flex flex-col gap-open-lg tablet:gap-open-xl desktop:gap-open-xl">
+      <div className={styles.personalInfoWrapper}>
+        <div className={styles.regWrapper}>
+          <FillingInput
+            labelText="소속"
+            inputType="text"
+            name="office"
+            placeholderText="회사 또는 부서를 입력해 주세요."
+          />
+        </div>
+        <div className={styles.regWrapper}>
+          <FillingInput
+            labelText="성명"
+            inputType="text"
+            name="name"
+            placeholderText="성함을 입력해 주세요."
+          />
+        </div>
       </div>
-      <div className={styles.regWrapper}>
-        <FillingInput
-          labelText="성명"
-          inputType="text"
-          name="name"
-          placeholderText="성함을 입력해 주세요."
-        />
-      </div>
-      <div className={styles.regWrapper}>
-        <FillingInput
-          labelText="받으실 이메일"
-          inputType="email"
-          name="email"
-          placeholderText="이메일을 입력해 주세요."
-          onChange={(e) => {
-            setEmailReg(e.target.value);
-          }}
-        />
-        {emailCheck.test(emailReg) || emailReg === '' ? (
-          ''
-        ) : (
-          <span className={styles.message}>유효한 이메일을 입력해 주세요.</span>
-        )}
-      </div>
-      <div className={styles.regWrapper}>
-        <FillingInput
-          labelText="받으실 연락처"
-          inputType="tel"
-          name="tel"
-          placeholderText="'-' 없이 입력해 주세요."
-          maxLength="13"
-          value={telNumber}
-          onChange={(e) => {
-            setTelNumber(
-              e.target.value
-                .replace(/[^0-9]/g, '')
-                .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, '$1-$2-$3')
-                .replace(/(\-{1,2})$/g, ''),
-            );
-          }}
-        />
+      <div className={styles.personalInfoWrapper}>
+        <div className={styles.regWrapper}>
+          <FillingInput
+            labelText="받으실 이메일"
+            inputType="email"
+            name="email"
+            placeholderText="이메일을 입력해 주세요."
+            onChange={(e) => {
+              setEmailReg(e.target.value);
+            }}
+          />
+          {emailCheck.test(emailReg) || emailReg === '' ? (
+            ''
+          ) : (
+            <span className={styles.message}>유효한 이메일을 입력해 주세요.</span>
+          )}
+        </div>
+        <div className={styles.regWrapper}>
+          <FillingInput
+            labelText="받으실 연락처"
+            inputType="tel"
+            name="tel"
+            placeholderText="'-' 없이 입력해 주세요."
+            maxLength="13"
+            value={telNumber}
+            onChange={(e) => {
+              setTelNumber(
+                e.target.value
+                  .replace(/[^0-9]/g, '')
+                  .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, '$1-$2-$3')
+                  .replace(/(\-{1,2})$/g, ''),
+              );
+            }}
+          />
+        </div>
       </div>
     </fieldset>
   );
