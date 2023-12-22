b18aa958fccc69d57aef6a250784ad09c1aa8f0f
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2023-12-22 17:18:16 +0900

    🌈 [Design] : 메인페이지 - Universe 컴포넌트 aos 적용
    Universe 컴포넌트에 aos 라이브러리를 적용하여 스크롤에 따라 애니메이션 효과를 주었습니다.
    
    Related to #189

--- src/components/Home/Universe.jsx
+++ src/components/Home/Universe.jsx
@@ -7,8 +7,12 @@ function Universe() {
   return (
     <section className={styles.universe} style={{ backgroundImage: `url('${BgImg}')` }}>
       <div className={styles.titleWrapper}>
-        <p className={styles.subtitle}>Explore OpenSG</p>
-        <h3 className={styles.title}>OPENSG의 세계관을 만나보세요</h3>
+        <p className={styles.subtitle} data-aos="fade-up">
+          Explore OpenSG
+        </p>
+        <h3 className={styles.title} data-aos="fade-up" data-aos-delay="100">
+          OPENSG의 세계관을 만나보세요
+        </h3>
       </div>
       <Keywords />
     </section>
