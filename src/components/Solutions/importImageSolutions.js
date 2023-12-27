572161f7982175147ec18b3dbfd1fef924e163f9
ryudg <ryudg95@gmail.com>
2023-12-27 15:58:02 +0900

    ✨ [Feat] : Banner 이미지 변경 및 Simulator 이미지 추가 - Banner 이미지 변경 - Simulator 이미지 추가
    Github issue #239

--- src/components/Solutions/importImageSolutions.js
+++ src/components/Solutions/importImageSolutions.js
@@ -1,13 +1,13 @@
-import ACS from '@/assets/products_ACS.png';
-import OCS from '@/assets/products_OCS.png';
-import SCS from '@/assets/products_SCS.png';
-import CCS from '@/assets/products_CCS.png';
-import IDX from '@/assets/products_IDX Controller.png';
-import Map from '@/assets/products_Map Designer.png';
-import ACS_S from '@/assets/products_ACS Simulator.png';
-import SCS_S from '@/assets/products_SCS Simulator.png';
-import AIMS from '@/assets/products_AIMS.png';
-import HSMS from '@/assets/products_HSMS.png';
+import ACS from '@/assets/products_ACS_Icon.png';
+import OCS from '@/assets/products_OCS_Icon.png';
+import SCS from '@/assets/products_SCS_Icon.png';
+import CCS from '@/assets/products_CCS_Icon.png';
+import IDX from '@/assets/products_IDX Controller_Icon.png';
+import Map from '@/assets/products_Map Designer_Icon.png';
+import ACS_S from '@/assets/products_ACS Simulator_Icon.png';
+import SCS_S from '@/assets/products_SCS Simulator_Icon.png';
+import AIMS from '@/assets/products_AIMS_Icon.png';
+import HSMS from '@/assets/products_HSMS_Icon.png';
 
 export default {
   ACS,
