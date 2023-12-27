2bb8c1f0b343c0036ed455b0dfd8f323755dc68b
dani-day <allkite5122@gmail.com>
2023-12-27 22:01:55 +0900

    ✨ [Feat] : mobile 사이트맵에 aos 애니메이션 추가

--- src/components/Header/SitemapMobile.jsx
+++ src/components/Header/SitemapMobile.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import AOS from 'aos';
 import styles from '@/components/Header/Sitemap.module.css';
 import AboutUsList from '@/components/Header/AboutUsList';
 import ServiceAndSolution from '@/components/Header/ServiceAndSolution';
 import SupportList from '@/components/Header/SupportList';
 
 function SitemapMobile() {
+  AOS.init();
   const [expanded, setExpanded] = useState('none');
   return (
-    <div className="tablet:hidden desktop:hidden block absolute left-0 top-0 pt-[40px] w-full h-full overflow-auto">
+    <div data-aos="fade-in" data-aos-offset="10" data-aos-delay="100" data-aos-duration="1000" className="tablet:hidden desktop:hidden block absolute left-0 top-0 pt-[40px] w-full h-full overflow-auto">
       <ul className={styles.sitemapListMobile}>
         <AboutUsList accName="aboutus" expanded={expanded} setExpanded={setExpanded} />
         <ServiceAndSolution accName="service" expanded={expanded} setExpanded={setExpanded} />
