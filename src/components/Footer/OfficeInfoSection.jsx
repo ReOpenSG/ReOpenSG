2c9e20137e8c90ffa204517a5c371867a2bf573e
Chi Hyun Noh <101504272+JuniorTunarr@users.noreply.github.com>
2024-01-01 23:32:54 +0900

    ✨ [Feat] : footer 한영 변환 기능 추가
    Header의 한영 변환 버튼에 따라 footer의 텍스트가 한<->영으로 변환되도록 하였습니다.
    
    Related to #265

--- src/components/Footer/OfficeInfoSection.jsx
+++ src/components/Footer/OfficeInfoSection.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from '@/styles/styles.module.css';
+import { useTranslation } from 'react-i18next';
 
 function OfficeInfoSection() {
+  const { t } = useTranslation('footer');
   return (
     <section className={styles.officeWrapper}>
       <span className="sr-only">회사정보</span>
       <address className={`${styles.info} ${styles.text}`}>
-        <span>경기도 화성시 동탄첨단산업1로 51-9 M타워 1201호</span>
+        <span>{t('address')}</span>
         <div>
           <span>T. 031-373-2612</span>
           <span>F. 031-377-2615</span>
